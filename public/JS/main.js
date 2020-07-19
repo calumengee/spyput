bb3013cbbe0b5aa6deae64804bbc6fc21379eeed
Calum Nguyen <calumengee@calums-air.lan>
2020-07-19 16:26:20 -0700

    Change everything to Morse

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,32 +1,50 @@
-var detect = [];
-const dict = {'sp': 'support level at $'};
+var morse = [];
+var decoded = [];
+var temp = '';
+var decodeMorse = {
+    oi:"a",
+    iooo:"b",
+    ioio: "c",
+    ioo: "d",
+    o: "e",
+    ooio: "f",
+    iio: "g",
+    oooo: "h",
+    oo: "i",
+    oiii: "j",
+    ioi: "k",
+    oioo: "l",
+    ii: "m",
+    io: "n",
+    iii: "o",
+    oiio: "p",
+    iioi: "cute",
+    oio: "r",
+    ooo: "s",
+    i: "t",
+    ooi: "u",
+    oooi: "v",
+    oii: "w",
+    ioii: "y",
+    iioo: "z"
+    }
 
 $('.textfield').keydown(function(e){
-
-  const key = e.key || e.which;
-  const keyCode = e.keyCode || e.which;
-  const trigger = {' ': " "};
-
-
-  if (keyCode === 8) {
-    if (detect.length === 0) {
-      $('.section-heading').text("Nothin' yet to be forgotten...");
-    } else {
-      detect.splice(detect.length-1, 1);
-      $('.section-heading').text(detect.join(''));
-    }
-  } else {
-    detect.push(key);
-    $('.section-heading').text(detect.join(''));
-  }
-
-  if (key === trigger[key]) {
-    detect.splice(0, detect.length);
-    detect.push(dict['sp']);
-    $('.section-heading').text(detect.join(''));
-  } else {
-
-  }
-
+  if (e.keyCode === 73 || e.keyCode === 79) {
+       morse.push(e.key);
+   } else if (e.keyCode === 32) {
+       temp = decodeMorse[morse.join('')];
+       morse = [];
+       decoded.push(temp);
+       $('.section-heading').text(decoded.join(""));
+   } else if (e.keyCode === 8) {
+       morse = [];
+       decoded = [];
+       temp = '';
+   } else if (e.keyCode === 13) {
+       morse = [];
+       temp = "";
+       decoded.push(" ");
+   }
 
 });
