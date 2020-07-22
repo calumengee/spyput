0e6b2234dacf711ffd4200543754117525f754d8
Calum Nguyen <calumengee@calums-air.lan>
2020-07-21 20:01:18 -0700

    clean tree before deploy

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static("public"));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+mongoose.connect('mongodb://localhost:27017/morseDB', {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+const morseSchema = new mongoose.Schema ({
+  morse: String
+});
 
 app.get('/', function(req, res){
 res.render('index');
